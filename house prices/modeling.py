6bdb7ae70469291ce616f0a1955d52d8973fac3e
XDCSHZR <26748904@163.com>
2018-08-20 02:28:13 +0800

    建模预测

--- house prices/modeling.py
+++ house prices/modeling.py
@@ -20,13 +20,30 @@ from sklearn.metrics import mean_squared_error
 from sklearn.externals import joblib
 
 
-
-# 模型评估，交叉验证
+# 模型评估，交叉验证, rmse分数越小越好
 def rmsle_cv(model, n_folds, x, y):
     kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(x)
     rmse= np.sqrt(-cross_val_score(model, x, y, scoring="neg_mean_squared_error", cv = kf))
     return(rmse)
 
+   
+# 平均(flag=0)/加权平均(flag=1)基本模型
+class averageBaseModel(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, models, scores):
+        self.models = models
+        self.scores = scores
+        
+    def fit(self, X, y):
+        self.models_ = [clone(x) for x in self.models]
+        for model in self.models_:
+            model.fit(X, y)
+        return self
+    
+    def predict(self, X, flag=1):
+        predictions = np.column_stack([model.predict(X) for model in self.models_])
+        return np.mean(predictions, axis=1) if flag==0 else np.average(predictions, axis=1, weights=[1-x for x in self.scores])
+            
+    
 if __name__ == '__main__':
     # 读取处理后的数据
     train = pd.read_csv('train.csv')
@@ -38,34 +55,38 @@ if __name__ == '__main__':
     # 网格搜索+交叉验证
     # 'alpha':0.0005, score:0.12372815776444199
 #    clf_lasso = make_pipeline(RobustScaler(), Lasso(alpha=0.0005, random_state=1))
+#    best_score_lasso = 0.12372815776444199
+    print('lasso...')
     best_score_lasso = float('inf');
     best_para_lasso = {};
     for a in [0.0005, 0.001, 0.01, 0.1, 1, 10, 100]:
         clf_lasso = make_pipeline(RobustScaler(), Lasso(alpha=a, random_state=1))
-        score_clf_lasso = rmsle_cv(clf_lasso, 5, train.values, y_train.values)
+        score_clf_lasso = rmsle_cv(clf_lasso, 5, train.values, y_train.values.ravel())
         score = score_clf_lasso.mean()
         print(a, ' score:', score)
         if score < best_score_lasso:
             best_score_lasso = score
             best_para_lasso = {'alpha': a}
-    
+   
     clf_lasso = make_pipeline(RobustScaler(), Lasso(**best_para_lasso, random_state=1))
-    clf_lasso.fit(train.values, y_train.values)
+    clf_lasso.fit(train.values, y_train.values.ravel())
     # “SalePrice”之前经过log(1+x)
     clf_lasso_pred = np.expm1(clf_lasso.predict(test.values))
     # 模型持久化
     joblib.dump(clf_lasso, "train_model_lasso.m")
-    clf_lasso = joblib.load("train_model_lasso.m")
+#    clf_lasso = joblib.load("train_model_lasso.m")
     
     # --------------------------------ENet-------------------------------------
     # 'alpha':0.001, l1_ratio:0.5, score:0.12355203588621444
 #    clf_ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.001, l1_ratio=0.5, random_state=3))
+#    best_score_ENet = 0.12355203588621444
+    print('ENet...')
     best_score_ENet = float('inf');
     best_para_ENet = {};
     for a in [0.0005, 0.001, 0.01, 0.1, 1, 10, 100]:
         for l1r in [0.9, 0.8, 0.7, 0.6, 0.5]:
             clf_ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=a, l1_ratio=l1r, random_state=3))
-            score_clf_ENet = rmsle_cv(clf_ENet, 5, train.values, y_train.values)
+            score_clf_ENet = rmsle_cv(clf_ENet, 5, train.values, y_train.values.ravel())
             score = score_clf_ENet.mean()
             print(a, ' ', l1r, ' score:', score)
             if score < best_score_ENet:
@@ -73,13 +94,15 @@ if __name__ == '__main__':
                 best_para_ENet = {'alpha': a, 'l1_ratio':l1r}
                 
     clf_ENet = make_pipeline(RobustScaler(), ElasticNet(**best_para_ENet, random_state=3))
-    clf_ENet.fit(train.values, y_train.values)
+    clf_ENet.fit(train.values, y_train.values.ravel())
     clf_ENet_pred = np.expm1(clf_ENet.predict(test.values))
     joblib.dump(clf_ENet, "train_model_ENet.m")
     
     # ---------------------------------SVR-------------------------------------
      # 'C':1, 'gamma':0.01, score:0.14908849519032602
 #    clf_SVR = make_pipeline(RobustScaler(), SVR(kernel='rbf', C=1, gamma=0.01)) 
+#    best_score_SVR = 0.14908849519032602
+    print('SVR...')
     best_score_SVR = float('inf');
     best_para_SVR = {};
     for c in [0.01, 0.5, 0.1, 1, 5, 10, 100]:
@@ -101,6 +124,8 @@ if __name__ == '__main__':
     # 'n_estimators':400, 'max_depth':5, 'min_samples_split':50, 'min_samples_leaf':20, score:0.12474860174235176
 #    clf_GBR = make_pipeline(RobustScaler(), GradientBoostingRegressor(n_estimators=400, learning_rate=0.1, max_depth=5, max_features='sqrt', 
 #                            min_samples_leaf=20, min_samples_split=50, loss='huber', random_state=5))
+#    best_score_GBR = 0.12474860174235176
+    print('GBR...')
     best_score_GBR_1 = float('inf');
     best_para_GBR_1 = {};
     for ne in range(100, 1100, 100):
@@ -132,6 +157,7 @@ if __name__ == '__main__':
     # 'max_depth':5, 'min_samples_split':50, 'min_samples_leaf':20, score:0.12474860174235176
     
     clf_GBR = make_pipeline(RobustScaler(), GradientBoostingRegressor(**best_para_GBR_1, **best_para_GBR_2, learning_rate=0.1,  max_features='sqrt', loss='huber', random_state=5))
+    best_score_GBR = 0.12474860174235176
     clf_GBR.fit(train.values, y_train.values.ravel())
     clf_GBR_pred = np.expm1(clf_GBR.predict(test.values))
     joblib.dump(clf_GBR, "train_model_GBR.m")
@@ -140,6 +166,8 @@ if __name__ == '__main__':
     # 'n_estimators':200, 'max_depth':5, 'min_samples_split':50, 'min_samples_leaf':10, score:0.17941187986702664
 #    clf_RFR = make_pipeline(RobustScaler(), RandomForestRegressor(n_estimators=200, max_depth=5, max_features='sqrt', 
 #                            min_samples_leaf=10, min_samples_split=50, random_state=7))
+#    best_score_RFR = 0.17941187986702664
+    print('RFR...')
     best_score_RFR_1 = float('inf');
     best_para_RFR_1 = {};
     for ne in range(100, 1100, 100):
@@ -171,6 +199,7 @@ if __name__ == '__main__':
     # 'max_depth':5, 'min_samples_split':50, 'min_samples_leaf':10, score:0.17941187986702664
     
     clf_RFR = make_pipeline(RobustScaler(), RandomForestRegressor(**best_para_RFR_1, **best_para_RFR_2, max_features='sqrt', random_state=7))
+    best_score_RFR = 0.17941187986702664
     clf_RFR.fit(train.values, y_train.values.ravel())
     clf_RFR_pred = np.expm1(clf_RFR.predict(test.values))
     joblib.dump(clf_RFR, "train_model_RFR.m")
@@ -179,6 +208,8 @@ if __name__ == '__main__':
     # 'n_estimators':900, 'max_depth':4, 'min_child_weight':3, 'gamma':0.0, 'subsample':0.8, 'colsample_bytree':0.6, 'reg_alpha':0.005, 'reg_lambda':1, score:0.12080521195456355
 #    clf_XGB = make_pipeline(RobustScaler(), xgb.XGBRegressor(colsample_bytree=0.6, gamma=0.0, learning_rate=0.1, max_depth=4, min_child_weight=3, n_estimators=900, 
 #                            reg_alpha=0.005, reg_lambda=1, subsample=0.8, silent=1, random_state=9))
+#    best_score_XGB = 0.12080521195456355
+    print('XGB...')
     best_score_XGB_1 = float('inf');
     best_para_XGB_1 = {};
     for ne in range(100, 1100, 100):
@@ -254,14 +285,17 @@ if __name__ == '__main__':
     
     clf_XGB = make_pipeline(RobustScaler(), xgb.XGBRegressor(**best_para_XGB_1, **best_para_XGB_2, **best_para_XGB_3, **best_para_XGB_4, **best_para_XGB_5, 
                             learning_rate=0.1, silent=1, random_state=9))
+    best_score_XGB = 0.12080521195456355
     clf_XGB.fit(train.values, y_train.values.ravel())
     clf_XGB_pred = np.expm1(clf_XGB.predict(test.values))
     joblib.dump(clf_XGB, "train_model_XGB.m")
     
     #----------------------------------LGB-------------------------------------
    # 'n_estimators':300, 'max_depth':3, 'num_leaves':5, 'max_bin':25, 'min_data_in_leaf':5, 'feature_fraction':0.5, 'bagging_fraction':0.9, 'bagging_freq':20, 'lambda_l1':0.1, 'lambda_l2':0.1, score:0.12246028009192222
-    clf_LGB = make_pipeline(RobustScaler(), lgb.LGBMRegressor(objective='regression', max_depth=3, num_leaves=5, learning_rate=0.1, n_estimators=300, max_bin=25, bagging_fraction=0.9, bagging_freq=20, 
-                            feature_fraction=0.5, feature_fraction_seed=11, bagging_seed=11, min_data_in_leaf=5, lambda_l1=0.1, lambda_l2=0.1))
+#    clf_LGB = make_pipeline(RobustScaler(), lgb.LGBMRegressor(objective='regression', max_depth=3, num_leaves=5, learning_rate=0.1, n_estimators=300, max_bin=25, bagging_fraction=0.9, bagging_freq=20, 
+#                            feature_fraction=0.5, feature_fraction_seed=11, bagging_seed=11, min_data_in_leaf=5, lambda_l1=0.1, lambda_l2=0.1))
+#    best_score_LGB = 0.12246028009192222
+    print('LGB...')
     best_score_LGB_1 = float('inf');
     best_para_LGB_1 = {};
     for ne in range(100, 1100, 100):
@@ -339,17 +373,35 @@ if __name__ == '__main__':
             
     clf_LGB = make_pipeline(RobustScaler(), lgb.LGBMRegressor(**best_para_LGB_1, **best_para_LGB_2, **best_para_LGB_3, **best_para_LGB_4, **best_para_LGB_5, 
                             objective='regression', learning_rate=0.1, feature_fraction_seed=11, bagging_seed=11))
+    best_score_LGB = 0.12246028009192222
     clf_LGB.fit(train.values, y_train.values.ravel())
     clf_LGB_pred = np.expm1(clf_LGB.predict(test.values))
     joblib.dump(clf_LGB, "train_model_LGB.m")
     
     #-------------------------------集成融合------------------------------------
+    # 基本模型平均/加权平均融合
+    # model:['lasso', 'ENet', 'SVR', 'GBR'], score:0.1205827216025805
+#    best_score_fusionModel = 0.12472772139487891
+#    best_score_fusionModel = 0.1205827216025805
+    print('fusionModel...')
+#    baseModels = [clf_lasso, clf_ENet, clf_SVR, clf_GBR, clf_RFR]
+#    baseScores = [best_score_lasso, best_score_ENet, best_score_SVR, best_score_GBR, best_score_RFR]
+    baseModels = [clf_lasso, clf_ENet, clf_SVR, clf_GBR]
+    baseScores = [best_score_lasso, best_score_ENet, best_score_SVR, best_score_GBR]
+    clf_fusionModel = averageBaseModel(baseModels, baseScores)
+    score_clf_fusionModel = rmsle_cv(clf_fusionModel, 5, train.values, y_train.values.ravel())
+    best_score_fusionModel = score_clf_fusionModel.mean()
+    clf_fusionModel.fit(train.values, y_train.values.ravel())
+    clf_fusionModel_pred = np.expm1(clf_fusionModel.predict(test.values))
+    joblib.dump(clf_fusionModel, "train_model_fusion.m")
+    
+    # stacking
     
     
     #-------------------------------提交结果------------------------------------
     submission = pd.DataFrame()
     submission['Id'] = old_test['Id']
-    submission['SalePrice'] = clf_XGB_pred
+    submission['SalePrice'] = clf_fusionModel_pred
     submission.to_csv('submission.csv', index=False)
 
 
